7fd8d79c4f6f6c674f1d2928685201472e760a94
justinseibert <justinseibert@users.noreply.github.com>
2017-07-26 09:36:16 -0700

    prototype was a bad idea

--- src/js/circumplexus.js
+++ src/js/circumplexus.js
@@ -1,7 +1,7 @@
 var Circumplexus = function(format,container){
 	// "use strict";
-	var s = this || {};
-	s.init = {
+	var cpx = this || {};
+	cpx.def = {
 		'strokeSVG'	: {
 			'solid' : 'stroke:rgba(150,150,150,1);stroke-width:1;stroke-linecap:butt;stroke-dasharray:none;',
 			'dash'  : 'stroke:rgba(150,150,150,1);stroke-width:1.5;stroke-linecap:butt;stroke-dasharray:5,5;',
@@ -41,28 +41,28 @@ var Circumplexus = function(format,container){
 		'format'        : format,
 		'container'     : (container == undefined) ? 'results' : container,
 	}
-	s.update = function(segment, collection){
-		s.init[collection].push(segment);
-		s.init.the_shape.push(segment);
-		s.init.the_data.push(s.data(segment));
+	cpx.update = function(segment, collection){
+		cpx.def[collection].push(segment);
+		cpx.def.the_shape.push(segment);
+		cpx.def.the_data.push(cpx.data(segment));
 	},
-	s.data = function(coords){
+	cpx.data = function(coords){
 		var data = {
-			'x0'        : coords[0][s.init.x],
-			'y0'        : coords[0][s.init.y],
-			'x1'        : coords[1][s.init.x],
-			'y1'        : coords[1][s.init.y],
-			'distance'  : s.geom.distance(coords),
-			'axis'      : s.geom.axis(coords),
-			'angle'     : s.geom.angle(coords),
-			'delta'     : s.geom.delta(coords),
-			'direction' : s.geom.direction(coords),
-			'midpoint'  : s.geom.midpoint(coords),
+			'x0'        : coords[0][cpx.def.x],
+			'y0'        : coords[0][cpx.def.y],
+			'x1'        : coords[1][cpx.def.x],
+			'y1'        : coords[1][cpx.def.y],
+			'distance'  : cpx.geom.distance(coords),
+			'axis'      : cpx.geom.axis(coords),
+			'angle'     : cpx.geom.angle(coords),
+			'delta'     : cpx.geom.delta(coords),
+			'direction' : cpx.geom.direction(coords),
+			'midpoint'  : cpx.geom.midpoint(coords),
 		};
-		s.relate.bounds([data.x0, data.x1], [data.y0, data.y1]);
+		cpx.relate.bounds([data.x0, data.x1], [data.y0, data.y1]);
 		return data;
 	},
-	s.process = {
+	cpx.process = {
 		inputs: function(){
 			var settings = {};
 		  $('input.vector', $('#settings')).each(function() {
@@ -75,14 +75,14 @@ var Circumplexus = function(format,container){
 				settings.bump.push(100);
 			}
 			console.log(settings);
-		  s.process.data(settings);
+		  cpx.process.data(settings);
 		},
 	  data: function(settings){
-	    var base = s.geom.radian(settings.base);
+	    var base = cpx.geom.radian(settings.base);
 	    var grain = settings.grain;
 	    var bump = settings.bump;
-	    // s.init.image = settings.image;
-	    s.init.temple = settings.temple;
+	    // cpx.def.image = settings.image;
+	    cpx.def.temple = settings.temple;
 	    var angle = (2*Math.PI - base)/(grain-1);
 
 			var data = [];
@@ -113,20 +113,20 @@ var Circumplexus = function(format,container){
 				d.distance.radius = bump[i]
 				d.theta.origin = base/2 + angle*i;
 				d.theta.radius = (i == 0 || i == grain) ? base : angle;
-				d.x = s.init.center + d.distance.radius * Math.cos(d.theta.origin)
-				d.y = s.init.center + d.distance.radius * Math.sin(d.theta.origin);
+				d.x = cpx.def.center + d.distance.radius * Math.cos(d.theta.origin)
+				d.y = cpx.def.center + d.distance.radius * Math.sin(d.theta.origin);
 			}
 
 			for(var i in data){
 				var current  = data[i];
 				var previous = (i > 0) ? data[parseInt(i)-1] : data[data.length-1];
-				current.distance.perimeter = s.geom.lawofcosines(current.distance.radius, current.theta.radius, previous.distance.radius);
+				current.distance.perimeter = cpx.geom.lawofcosines(current.distance.radius, current.theta.radius, previous.distance.radius);
 				// determine the shorter side -> angle for law of sines
 				if(current.distance.radius < previous.distance.radius){
-					current.theta.advance = s.geom.lawofsines(current.distance.perimeter, current.theta.radius, current.distance.radius, false);
+					current.theta.advance = cpx.geom.lawofsines(current.distance.perimeter, current.theta.radius, current.distance.radius, false);
 					current.theta.return = Math.PI - (current.theta.radius + current.theta.advance);
 				} else {
-					current.theta.return = s.geom.lawofsines(current.distance.perimeter, current.theta.radius, previous.distance.radius, false);
+					current.theta.return = cpx.geom.lawofsines(current.distance.perimeter, current.theta.radius, previous.distance.radius, false);
 					current.theta.advance = Math.PI - (current.theta.radius + current.theta.return);
 				}
 			}
@@ -135,13 +135,12 @@ var Circumplexus = function(format,container){
 				var current  = data[i];
 				var next     = (parseInt(i) < data.length-1) ? data[parseInt(i)+1] : data[0];
 				current.theta.across = current.theta.return + next.theta.advance;
-				if( current.theta.across < s.geom.radian(s.init.reduce.min) || current.theta.across > s.geom.radian(s.init.reduce.max) || i == 0 || i == data.length-1){
+				if( current.theta.across < cpx.geom.radian(cpx.def.reduce.min) || current.theta.across > cpx.geom.radian(cpx.def.reduce.max) || i == 0 || i == data.length-1){
 					data_splice.push(data[i]);
 				}
 			}
-	    s.init.offset = data_splice[0].distance.perimeter/2;
-			console.log(data_splice);
-			s.process.starter(data_splice.reverse());
+	    cpx.def.offset = data_splice[0].distance.perimeter/2;
+			cpx.process.starter(data_splice.reverse());
 	  },
 	  starter: function(data){
 			var starter  = [];
@@ -152,48 +151,48 @@ var Circumplexus = function(format,container){
 	      starter.push(data[a].x,data[a].y,data[b].x,data[b].y);
 	      points.push(data[i].x);
 	    }
-			// console.log('temple: ', s.init.temple, s.init.offset);
-	    var distance = s.geom.maximum(points) - s.geom.minimum(points);
+			// console.log('temple: ', cpx.def.temple, cpx.def.offset);
+	    var distance = cpx.geom.maximum(points) - cpx.geom.minimum(points);
 			// console.log(points, distance);
 			// correction to prevent flattening on angle
-			if( s.init.offset < distance ){
-				s.init.temple = s.init.temple * s.geom.lawofsines(distance, s.geom.radian(90), s.init.offset, false);
+			if( cpx.def.offset < distance ){
+				cpx.def.temple = cpx.def.temple * cpx.geom.lawofsines(distance, cpx.geom.radian(90), cpx.def.offset, false);
 			} else {
-				s.init.temple = s.init.temple * s.geom.lawofsines(distance, s.geom.radian(90), distance*0.99, false);
+				cpx.def.temple = cpx.def.temple * cpx.geom.lawofsines(distance, cpx.geom.radian(90), distance*0.99, false);
 			}
-			console.log(starter);
-	    s.process.shape(starter)
+	    cpx.process.shape(starter)
 		},
 		shape: function(data){
+			console.log(data);
 			var print_page = document.createElement('div');
 			print_page.className = 'page-size';
 			document.body.appendChild(print_page);
-			s.init.inch = print_page.offsetWidth/8.5;
+			cpx.def.inch = print_page.offsetWidth/8.5;
 
 			for(var i = 0; i < data.length-3; i+=4) {
 				var segment = [
 					[ data[i],  data[i+1] ],
 					[ data[i+2], data[i+3] ],
 				];
-				s.update(segment,'the_base');
+				cpx.update(segment,'the_base');
 			}
-			s.relate.surround(s.init.the_shape);
-			s.relate.mirror(s.init.the_shape, 'vertical', 0, 'the_mirror');
-			s.relate.connect(s.init.the_shape);
-			s.relate.tabs();
-			s.relate.mirror(s.init.the_tabs, 'vertical', 0, 'the_tabs');
+			cpx.relate.surround(cpx.def.the_shape);
+			cpx.relate.mirror(cpx.def.the_shape, 'vertical', 0, 'the_mirror');
+			cpx.relate.connect(cpx.def.the_shape);
+			cpx.relate.tabs();
+			cpx.relate.mirror(cpx.def.the_tabs, 'vertical', 0, 'the_tabs');
 
-			if(s.init.format == 'wireframe'){
-				s.draw.PNG(this);
-			} else if(s.init.format == 'png'){
+			if(cpx.def.format == 'wireframe'){
+				cpx.draw.PNG(this);
+			} else if(cpx.def.format == 'png'){
 				var img = new Image();
-				img.src = s.init.image;
+				img.src = cpx.def.image;
 				img.onload = function(){
 					print_page.remove();
-					s.draw.PNG(this);
+					cpx.draw.PNG(this);
 				}
 			} else {
-				s.draw.SVG();
+				cpx.draw.SVG();
 			}
 		}
 	}
